2dd58edea615a7b02be661f272e10e2bf130e486
[danie] <danielsantiagosalavarrietai@gmail.com>
2025-10-17 07:47:06 -0500

    Agrego solución del ejercicio 118: Triángulo de Pascal

---
+++ pascal_triangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> triangle(numRows);
+
+    for (int i = 0; i < numRows; ++i) {
+        triangle[i].resize(i + 1);
+        triangle[i][0] = triangle[i][i] = 1;
+
+        for (int j = 1; j < i; ++j)
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    }
+    return triangle;
+}
+
+int main() {
+    int numRows;
+    cout << "Ingrese el número de filas: ";
+    cin >> numRows;
+
+    vector<vector<int>> result = generate(numRows);
+
+    for (const auto &row : result) {
+        for (int num : row)
+            cout << num << " ";
+        cout << endl;
+    }
+    return 0;
+}
